4314243aa75d8ca07736beab01a36ff016971d79
guilherme.nsantos7 <guilherme_nsantos2002@hotmail.com>
2020-03-03 12:21:35 -0300

    Bandio

---
+++ app/src/main/java/br/com/sp/senac/bandio/Sign_up_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.bandio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_up_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_layout);
+    }
+}
